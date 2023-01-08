01e60ba3cf1cdec38a4db2e4d1d68f36ae5ea2fd
Ning Li <lining04111223@gmail.com>
2023-01-08 10:45:02 -0800

    type

---
+++ dataType.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+char grade = 'A';
+string phrase = "gggg";
+int age =50;
+double gpa =2.3;
+bool isMale = false;
+
+
+return 0;
+}
---
+++ welcome.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+string characterName ="John";
+int characterAge;
+characterAge=35;
+cout <<" My name is"<<characterName<<endl;
+cout <<" I am " <<characterAge  << "years old"<<endl;
+
+
+return 0;
+
+}
